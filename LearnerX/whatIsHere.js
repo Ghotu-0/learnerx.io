80aec2ae124f669dc26bdf819d93f6024cb4c8c4
NikhilPunia01 <078universalhacker@gmail.com>
2021-04-29 05:09:37 +0530

    updated whatishere

---
+++ LearnerX/whatIsHere.js
@@ -0,0 +1,12 @@
+/* Here are all the main files/folders avalable for LernerX */
+/* <------------------------------------------------------> */
+
+class ManageApp{
+
+refrenceStorage={
+    javascript:"scripts",
+    css:"stylesheet",
+    html:"structure",
+        }     
+
+                }

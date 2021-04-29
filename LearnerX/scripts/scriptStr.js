52f89fdc53b7e728b63f8be4aa34d02bb922607f
NikhilPunia01 <078universalhacker@gmail.com>
2021-04-29 16:36:35 +0530

    updated

--- LearnerX/scripts/scriptStr.js
+++ LearnerX/scripts/scriptStr.js
@@ -1,12 +1,30 @@
 class ScriptStructure{
-    file="D:/learnerx.io/LearnerX/scripts/ScriptStructure.json";
+
+    file="https://github.com/NikhilPunia01/learnerx.io/blob/aac8758c440ee0f79ded29e00af14ae2dc0270ec/LearnerX/scripts/ScriptStructure.json";
+
+    constructor(filex){
+    if (filex) {
+      this.file=filex;
+    }
+    }
 
     async log() {
-        let myObject = await fetch(this.file);
-        let myText = await myObject.text();
-        console.log(myText);
-      }
+        try{
+        let fileObj = await fetch(this.file);
+        let json = await fileObj.text();
+ 
+        var logData = JSON.parse(json);
 
+        for (let l = 0; l < logData.Scripts.length; l++) {
+         let entry=logData.Scripts[l];
+         console.table(entry);
+        }
+       }catch(err){
+       console.log(err);
+       }
+
+      }
+      
 }
 
 let tryo=new ScriptStructure;
